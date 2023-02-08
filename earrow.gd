63fed5f7c3efc224e9da6478ed27c78db665c496
Classifiedone <jasonzabeckijr@outlook.com>
2023-02-08 03:09:58 -0500

    Livin On The High Life
    Not even gonna try

--- earrow.gd
+++ earrow.gd
@@ -1,4 +1,4 @@
-extends KinematicBody2D
+extends RigidBody2D
 
 
 # Declare member variables here. Examples:
@@ -8,6 +8,7 @@ extends KinematicBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	mode = MODE_STATIC
 	pass # Replace with function body.
 
 
--- parrow.gd
+++ parrow.gd
@@ -1,18 +1,31 @@
-extends KinematicBody2D
+extends RigidBody2D
 
 var mouse_position
 var is_fired = false
 
 func _ready():
-
+	mass = 4
+	mode = MODE_STATIC
 	pass
 	
 func _process(delta):
 	if Input.is_action_just_pressed("fire"):
 		mouse_position = get_global_mouse_position() - global_position
+		mode = MODE_RIGID
+		apply_impulse(Vector2.ZERO , Vector2(900,0))
 		is_fired = true
+		if linear_velocity.is_equal_approx(Vector2.ZERO):
+			is_fired = false
+		pass
+
 	if is_fired:
-		var collisions = move_and_collide(mouse_position/20)
-		if collisions != null:
-			queue_free()
+		pass
+
+func _physics_process(delta: float) -> void:
+	if is_fired:
+		look_at(position + linear_velocity)	
+		pass
+
+func _on_parrow_body_entered(body: Node) -> void:
+	queue_free()
 	pass
